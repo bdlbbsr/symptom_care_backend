91f8a787bc55e8716966a49e7c9d1c5909416c44
bdlbbsr <130879988+bdlbbsr@users.noreply.github.com>
2024-08-21 22:19:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,12 +27,12 @@ app.use(compression());
 // }));
 
 app.use(cors());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   next();
+// });
 // app.use(function(req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "https://symptom-care-frontend.vercel.app");
 //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
